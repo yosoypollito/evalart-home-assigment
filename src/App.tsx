bcbb846f8888c3df65d56cd69a150976cc85e53b
Daif <dev.daif@gmail.com>
2024-02-01 16:16:28 -0400

    First commit, Tailwind, Iconify/React, vite-tsconfig-paths, Json SERVER mock and command.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Icon } from '@iconify/react'
+function App() {
+
+  return (
+    <main className="grid h-screen w-full place-content-center">
+      WIP
+
+      <Icon icon="material-symbols:home" />
+    </main>
+  )
+}
+
+export default App
