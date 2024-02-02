b1f62dfc95a4db49b029b97e8f0daec19f6d4295
Daif <dev.daif@gmail.com>
2024-02-01 23:15:43 -0400

    types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Feature {
+  icon: string;
+  title: string;
+  description: string;
+  keywords: string[];
+  relationship: string;
+  id: string;
+}
+
+export interface Link {
+  href: string;
+  label: string;
+}
