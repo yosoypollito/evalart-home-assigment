bf0f36c32cd74bf6f5cef7abfc607fa53c197c85
Daif <dev.daif@gmail.com>
2024-02-01 23:17:39 -0400

    Link style changed to only when is a button

--- src/lib/ui/components/navigation/link.tsx
+++ src/lib/ui/components/navigation/link.tsx
@@ -2,18 +2,18 @@ import { HTMLProps } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface Link extends HTMLProps<HTMLAnchorElement> {
-  button: boolean;
+  button?: boolean;
   secondary?: boolean;
 }
 
 export default function Link(props: Link) {
 
-  const { children, button, secondary } = props;
+  const { children, button, secondary, className, ...rest } = props;
   return (
-    <a className={twMerge("py-2 px-4 font-semibold rounded-lg shadow-lg text-evalart-blue",
+    <a className={twMerge("font-semibold rounded-lg text-evalart-blue",
       secondary && "text-evalart-orange",
-      button && "bg-evalart-blue text-white",
-      (button && secondary) && "bg-evalart-orange")} {...props}>
+      button && "bg-evalart-blue text-white shadow-lg py-2 px-4 ",
+      (button && secondary) && "bg-evalart-orange", className)} {...rest}>
       {children}
     </a>
   )
