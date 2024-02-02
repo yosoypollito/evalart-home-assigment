da1e077d8fdfca51d6ebec48b3f1c8862011dec6
Daif <dev.daif@gmail.com>
2024-02-01 23:17:57 -0400

    Feature Page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Icon } from '@iconify/react'
+import Benefits from './lib/ui/benefits'
+import Features from './lib/ui/features'
+import Footer from './lib/ui/footer/footer'
+import WhyOurService from './lib/ui/header/why-our-service'
+import NavBar from './lib/ui/navbar/navbar'
 function App() {
 
   return (
-    <main className="grid h-screen w-full place-content-center">
-      WIP
+    <>
+      <NavBar />
 
-      <Icon icon="material-symbols:home" />
-    </main>
+      <WhyOurService />
+
+      <main className='max-w-content-width m-auto flex flex-col justify-center items-center p-2 md:gap-8 md:p-10'>
+        <Benefits />
+
+        <Features />
+      </main>
+      <Footer />
+    </>
   )
 }
 
