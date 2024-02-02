1454a5bba9a155cc288fed8ab294535f2ae354a2
Daif <dev.daif@gmail.com>
2024-02-01 23:16:22 -0400

    Loading component

---
+++ src/lib/ui/components/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+
+  return (
+    <div className="col-span-3 flex gap-2 items-center justify-center">
+      <div className="animate-spin h-5 w-5 border-2 border-evalart-blue border-t-evalart-orange rounded-full">
+      </div>
+      Loading...
+    </div>
+  )
+}
